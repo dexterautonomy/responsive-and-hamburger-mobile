d8f5d684ce45839b82b1c78eeaf25f6c18e91f16
dexterautonomy <chineduezeigbo2015@gmail.com>
2020-12-19 18:43:52 +0100

    Responsive...

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
+    var drawerIsDisplayed = false; //to track state of drawer
+    
     $('#drawer').click(function(){
         if($('#myModal').is(':visible')){
             $('#myModal').css("display", "none");
+            drawerIsDisplayed = false;
         }else{
             $('#myModal').css("display", "block");
+            drawerIsDisplayed = true;
         }
-            
-        
     });
     
     $(window).click(function(event){
@@ -14,6 +16,20 @@ $(document).ready(function(){
         if($(event.target).is($('#myModal'))){
             $('#myModal').css("display", "none");
         }
+    });
+    
+    
+    
+    $(window).resize(function(){
+        var screenWidth = $(window).width();
+        
+        if(screenWidth > 768){
+            $('#myModal').css("display", "none");
+        }
+        else if(drawerIsDisplayed){
+            $('#myModal').css("display", "block");
+        }
+        
     });
 });
 
